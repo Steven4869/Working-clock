3a63bbfb0357eacda15c1f31114a7377b218b092
Steven <maskirade.100@gmail.com>
2021-04-08 14:55:35 +0530

    Comitted

---
+++ clock.js
@@ -0,0 +1,23 @@
+
+		setInterval(() => {
+
+			d = new Date()
+			htime=d.getHours()
+			mtime=d.getMinutes()
+			stime=d.getSeconds()
+			hrotation=30*htime+ mtime/2;
+			mrotation=6*mtime;
+			srotation=6*stime
+			hour.style.transform=`rotate(${hrotation}deg)`;
+			minute.style.transform=`rotate(${mrotation}deg)`;
+			second.style.transform=`rotate(${srotation}deg)`;
+
+		}, 1000);
+		console.log("this is date and time")
+		let now=new Date()
+		console.log(now)
+		setInterval(updatetime,1000)
+		function updatetime(){
+			time.innerHTML=new Date()
+
+		}
